36c7604464fafff20627ff6a9fcda9bfe2e49ffe
Ray's Git <lancelot53.ra@gmail.com>
2021-12-10 15:27:35 -0500

    Completed

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
-require 'pry'
+require "pry"
 
 def my_find(collection)
-
+  i = 0
+  while collection.length > i
+    if yield(collection[i])
+    return collection[i]
+    end
+  #else
+    i += 1
+  end
 end
